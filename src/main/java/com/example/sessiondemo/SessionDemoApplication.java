08383a88a84407f6e94ba9aec00a99cc2bf84330
pengw00 <water685@hotmail.com>
2019-12-29 20:35:12 -0600

    add spring http session

---
+++ src/main/java/com/example/sessiondemo/SessionDemoApplication.java
@@ -0,0 +1,36 @@
+package com.example.sessiondemo;
+
+import java.security.Principal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.session.data.redis.config.ConfigureRedisAction;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.http.HeaderHttpSessionIdResolver;
+import org.springframework.session.web.http.HttpSessionIdResolver;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableWebSecurity
+@EnableRedisHttpSession
+@RestController
+public class SessionDemoApplication {
+
+	@GetMapping("/name")
+	public String getName(Principal principal) {
+		return principal.getName();
+	}
+
+	@Bean
+	public ConfigureRedisAction configureRedisAction() {
+		return ConfigureRedisAction.NO_OP;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionDemoApplication.class, args);
+	}
+
+}
